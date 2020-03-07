3139cdd7446e5729657029d01715cd0eb740f0ff
Alessandro <48725646+AndrewCortis@users.noreply.github.com>
2020-03-07 02:08:41 -0600

    Termino incompleto
    Revisión

--- navbar.php
+++ navbar.php
@@ -1,3 +1,11 @@
+<?php
+  include "include/conecta.php";
+  // consulta que se utiliza para extraer datos de mi BD
+  $query = "SELECT * FROM Productos ORDER BY Id_productos";
+  $ejecuta = $mysql->query($query);
+  $numero = $ejecuta->num_rows;
+  $mysql->close();
+?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
@@ -50,7 +58,7 @@
                 <a class="dropdown-item" href="#">Perfil</a>
                 <a class="dropdown-item" href="#">Configuración</a>
                 <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="#">Cerrar sesión</a>
+                <a class="dropdown-item" href="#" data-toggle="modal" data-target="#ModalCenter">Cerrar Sesión</a>
               </div>
             </li>
           </ul>
@@ -64,12 +72,101 @@
         <div class="modal-content">
           <div class="modal-header">
             <h5 class="modal-title" id="ModalCenterTitle">Cerrar Sesión</h5>
-            <button type="button" name="button"></button>
+            <button type="button" class="close" data-dismiss="modal" aria-tabel="Close"><span aria-hidden="true">&times;</span></button>
           </div>
+            <div class="modal-body">¿Deseas cerrar tu sesión?</div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+              <button type="button" class="btn btn-danger">Cerrar Sesión</button>
+            </div>
         </div>
       </div>
     </div>
     <!-- Fin de ventana modal -->
+    <div class="alert alert-primary" role="alert">
+    Great! Your connection is successfully.
+    </div>
+    <section>
+      <!-- Inicio de cards -->
+      <div class="container">
+        <div class="row">
+          <?php while($row = $ejecuta->fetch_array()) { ?>
+          <div class="col-sm">
+            <div class="card mb-3" style="max-widht: 580px">
+              <div class="row no-gutters">
+                <div class="col-md-4">
+                  <img src="img/img1.jpg" class="card-img" alt="Producto01">
+                </div>
+                <div class="col-md-8">
+                  <div class="card-body">
+                    <h5 class="card-title">Nombre del producto</h5>
+                    <p class="card-text">Descripcion:</p>
+                    <p class="card-text"><small class="text-muted">Precio:</small></p>
+                    <button type="button" name="btn-compra" class="btn btn-danger nav-link">Agregar al carrito</button>
+                  </div>
+                </div>
+              </div>
+              <div class="card-rooter text-muted">
+                Categoria:
+              </div>
+            </div>
+          </div>
+        <?php } ?>
+        </div>
+      </div>
+
+      <div class="container">
+        <div class="row">
+          <div class="col-sm">
+            <div class="card mb-3" style="max-widht: 580px">
+              <div class="row no-gutters">
+                <div class="col-md-4">
+                  <img src="img/<?php echo $row("Imagen");?>" class="card-img" alt="Producto01">
+                </div>
+                <div class="col-md-8">
+                  <div class="card-body">
+                    <h5 class="card-title"><?php echo $row("Nombre");?></h5>
+                    <p class="card-text">Descripcion:<?php echo $row("Descripcion");?></p>
+                    <p class="card-text"><small class="text-muted">Precio:<?php echo $row("Precio");?></small></p>
+                    <button type="button" name="btn-compra" class="btn btn-danger nav-link">Agregar al carrito</button>
+                  </div>
+                </div>
+              </div>
+              <div class="card-rooter text-muted">
+                Categoria:<?php echo $row("Id_categoria");?>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="container">
+        <div class="row">
+          <div class="col-sm">
+            <div class="card mb-3" style="max-widht: 580px">
+              <div class="row no-gutters">
+                <div class="col-md-4">
+                  <img src="img/img3.jpg" class="card-img" alt="Producto01">
+                </div>
+                <div class="col-md-8">
+                  <div class="card-body">
+                    <h5 class="card-title">Nombre del producto</h5>
+                    <p class="card-text">Descripcion:</p>
+                    <p class="card-text"><small class="text-muted">Precio:</small></p>
+                    <button type="button" name="btn-compra" class="btn btn-danger nav-link">Agregar al carrito</button>
+                  </div>
+                </div>
+              </div>
+              <div class="card-rooter text-muted">
+                Categoria:
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </section>
+    <!-- Termino de cards -->
     <script type="text/javascript" src="js/bootstrap.js"></script>
     <script type="text/javascript" src="jquery/jquery-3.4.1.min.js"></script>
   </body>
