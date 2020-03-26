685341814649a7947495d9e2ef17158a6cbe253d
Alessandro <48725646+AndrewCortis@users.noreply.github.com>
2020-03-26 16:00:50 -0600

    Login Completo
    Creación de Login con conexión a BD y Catalogo

---
+++ include/login.php
@@ -0,0 +1,28 @@
+<?php
+  session_start();
+  include '../include/conecta.php';
+  $usuario = $_POST['user'];
+  $password = md5($_POST['password']);
+//consulta que se utiliza para ingresar al sitema
+  $q = "SELECT * FROM usuarios WHERE NUsuario = '$usuario' and Password = '$password'";
+  if ($resultado = $mysqli->query($q)) {
+    while($row = $resultado->fetch_array()){
+      $userok = $row['NUsuario'];
+      $passwordok = $row['Password'];
+    }
+    $resultado->close();
+  }
+  $mysqli->close();
+  if (isset($usuario)&& isset($password)) {
+    if ($usuario == $userok && $password == $passwordok) {
+      $_SESSION['login']= TRUE;
+      $_SESSION['usuario'] = $usuario;
+      header("location:../catalogo.php");
+    }
+    else {
+      header("location:../index.php");
+    }
+  } else {
+    header("location:../index.php");
+  }
+?>
